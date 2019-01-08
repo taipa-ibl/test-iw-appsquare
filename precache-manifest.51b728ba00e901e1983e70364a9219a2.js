bd3bffeb98a96b17c02eff84ddd1cbe3ba02e9d1
taipa <taipa@blockchainlabs.asia>
2019-01-08 18:31:52 +0700

    Updates

---
+++ precache-manifest.51b728ba00e901e1983e70364a9219a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "57c9d3f90bc614d8b318",
+    "url": "/my-app/static/js/main.57c9d3f9.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/my-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "57c9d3f90bc614d8b318",
+    "url": "/my-app/static/css/main.1c2f76a2.chunk.css"
+  },
+  {
+    "revision": "6f2e4b4e84afd8be2d1174e76842bfea",
+    "url": "/my-app/index.html"
+  }
+];
